7f9c1c35d537603e5b6c69d415d3796cbdf3245d
Joris <joris.thiel@tum.de>
2023-11-21 17:44:10 +0100

    Example case for spectral density plot

---
+++ spectraldensity.m
@@ -0,0 +1,3 @@
+function spectraldensity(x)
+    stairs(sort(x), (1:length(x))/length(x))
+end
--- test.m
+++ test.m
@@ -1,10 +1,11 @@
 clear all
+
+% set up problem
 N = 30;
-A = LSexample(N, 0.1, 10e3, 0.6);
+A = LSexample(N, 0.1, 1e3, 0.6);
 b = ones(N, 1)/sqrt(N);
 
-figure(1)
-clf
+% compare convergence in exact and finite arithmetic 
 iter = 100;
 err_norm = zeros(iter, 1);
 err_norm_ro = zeros(iter, 1);
@@ -20,8 +21,26 @@ for i = 1:iter
     err_norm_ro(i) = err2'*A*err2;
 end
 
+figure(1)
+clf
 semilogy(1:iter, err_norm/xnorm)
 hold on
 semilogy(1:iter, err_norm_ro/xnorm)
 ylim([1e-8, 10])
-legend("CG", "CGRO")
+legend("CG (finite arithmetic)", "CGRO (exact arithmetic)")
+title("Convergence comparison in A-norm")
+
+% cumulative spectral density plots of eigenvalues and Ritz values
+[xcgro, flag, relres, iter, resvec, ritz10] = ConjugateGradientRO(A, b, 1e-6, 10);
+[xcgro, flag, relres, iter, resvec, ritz20] = ConjugateGradientRO(A, b, 1e-6, 20);
+
+figure(2)
+clf
+spectraldensity(diag(A))
+hold on
+spectraldensity(ritz10)
+spectraldensity(ritz20)
+xlim([0, 1e3])
+legend("eigenvalues", "Ritz values k=10", "Ritz values k=20")
+legend('Location','southeast')
+title("Eigenvalues and Ritz values (exact)")
